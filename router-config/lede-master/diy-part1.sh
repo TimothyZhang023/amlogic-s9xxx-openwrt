fdc362cc56311edb3917a4fbc11067687be9a945
Timothy Zhang <3147994+TimothyZhang023@users.noreply.github.com>
2021-10-22 13:34:39 +0800

    Update diy-part1.sh

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -13,6 +13,8 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 sed -i '$a src-git 4IceG https://github.com/4IceG/luci-app-socat' feeds.conf.default
 
 
